82237563147e4f30ecc65ecb48b63268e1362b23
Joshua Jung <joshua.p.jung@gmail.com>
2013-06-30 21:12:30 -0500

    Added data handling through AJAX

--- scripts/lib/main.js
+++ scripts/lib/main.js
@@ -1,27 +1,51 @@
+
 (function($) {
 
 
+	// initialize variables
+	var currentTurn = 0, data;
 
-	var currentTurn = 0;
 
 
 
+
+
+
+
+	// game logic
 
 	var init = function() {
 		// initialize the game data
 		console.log("intializing game...");
+		recieveData();
 	}
 
 	var startGame = function() {
-		while (!winCondition) {
-			executeTurn();
-		}
+		// while (!winCondition) {
+		// 	executeTurn();
+		// }
 	}
 
 	var executeTurn = function() {
 		currentTurn++;
 	}
 
+	var recieveData = function() {
+		
+		console.log("attempting to load game.json")
+		$.ajax({
+		  	url: './assets/game.json',
+		  	async: false,
+		  	dataType: 'json',
+		  	success: function (response) {
+		    	console.log("data loaded successfully");
+		    	console.log(response.gameName);
+		 	}
+		});
+	}
+
+
+	//game loop
 
 	$(document).ready(function() {
 		init();
