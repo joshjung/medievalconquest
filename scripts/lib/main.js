28f04917d8c2a28c959073a5009caaac719ab14b
Joshua Jung <joshua.p.jung@gmail.com>
2013-06-30 20:41:14 -0500

    Made game module & updated the view

--- scripts/lib/main.js
+++ scripts/lib/main.js
@@ -1,3 +1,33 @@
-$(document).ready(function() {
-	console.log("Starting the game...");
-});
+(function($) {
+
+
+
+	var currentTurn = 0;
+
+
+
+
+	var init = function() {
+		// initialize the game data
+		console.log("intializing game...");
+	}
+
+	var startGame = function() {
+		while (!winCondition) {
+			executeTurn();
+		}
+	}
+
+	var executeTurn = function() {
+		currentTurn++;
+	}
+
+
+	$(document).ready(function() {
+		init();
+		startGame();
+	});
+
+
+})(jQuery);
+
